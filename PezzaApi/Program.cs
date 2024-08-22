3a67dca3e84ac3891845a0e65b3db4f567c42e95
Terence Frade <terence.frade@entelect.co.za>
2024-08-22 11:39:01 +0200

    Configured CORS and modified swagger configuration.

--- PezzaApi/Program.cs
+++ PezzaApi/Program.cs
@@ -1,19 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
 // Add services to the container.
+services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI((c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pezza Api");
+        c.RoutePrefix = "swagger";
+    }));
 }
 
 app.UseHttpsRedirection();
--- PezzaApi/obj/Debug/net8.0/PezzaApi.AssemblyInfo.cs
+++ PezzaApi/obj/Debug/net8.0/PezzaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f424528aa61aa04ee5c7a60f78af01b56302767a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
