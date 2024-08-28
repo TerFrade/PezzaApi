04b198f8936371c2e66182789f525f859f1d909e
Terence Frade <terence.frade@entelect.co.za>
2024-08-28 11:04:32 +0200

    1234

--- PezzaApi/obj/Debug/net8.0/PezzaApi.AssemblyInfo.cs
+++ PezzaApi/obj/Debug/net8.0/PezzaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27993aa18d54f8f963b2b7a72da89799e274c31f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214c1fc56ec0a1065171769cddcf962b40e5cd46")]
 [assembly: System.Reflection.AssemblyProductAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PezzaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
